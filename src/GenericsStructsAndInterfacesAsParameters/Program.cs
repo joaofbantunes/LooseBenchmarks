653f7eb22ca76a59059f1f1fc7bddf8818a80ba9
João Antunes <joao@antunes.dev>
2021-09-28 18:25:35 +0100

    Update to .NET 6 and simplify benchmarks

--- src/GenericsStructsAndInterfacesAsParameters/Program.cs
+++ src/GenericsStructsAndInterfacesAsParameters/Program.cs
@@ -1,96 +1,70 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
-namespace GenericsStructsAndInterfacesAsParameters
+_ = BenchmarkRunner.Run<Benchmark>();
+
+[RankColumn]
+[MemoryDiagnoser]
+public class Benchmark
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var summary = BenchmarkRunner.Run<Benchmark>();
-        }
-    }
+    private static readonly InterfaceCaller InterfaceCaller = new InterfaceCaller();
+    private static readonly GenericCaller GenericCaller = new GenericCaller();
 
-    [RankColumn]
-    [MemoryDiagnoser]
-    public class Benchmark
-    {
-        [Benchmark(Baseline = true)]
-        public void InterfaceCallerWithClass()
-            => InterfaceCaller.Instance.Call(new ClassCallable(i => i > 0));
+    [Benchmark(Baseline = true)]
+    public void InterfaceCallerWithClass()
+        => InterfaceCaller.Call(new ClassCallable(i => i > 0));
 
-        [Benchmark]
-        public void InterfaceCallerWithStruct()
-            => InterfaceCaller.Instance.Call(new StructCallable(i => i > 0));
+    [Benchmark]
+    public void InterfaceCallerWithStruct()
+        => InterfaceCaller.Call(new StructCallable(i => i > 0));
 
-        [Benchmark]
-        public void GenericCallerWithClass()
-            => GenericCaller.Instance.Call(new ClassCallable(i => i > 0));
+    [Benchmark]
+    public void GenericCallerWithClass()
+        => GenericCaller.Call(new ClassCallable(i => i > 0));
 
-        [Benchmark]
-        public void GenericCallerWithStruct()
-            => GenericCaller.Instance.Call(new StructCallable(i => i > 0));
-    }
+    [Benchmark]
+    public void GenericCallerWithStruct()
+        => GenericCaller.Call(new StructCallable(i => i > 0));
+}
 
-    public interface ICallable
-    {
-        bool Call(int i);
-    }
+public interface ICallable
+{
+    bool Call(int i);
+}
 
-    public class ClassCallable : ICallable
-    {
-        private readonly Func<int, bool> _innerCallable;
+public class ClassCallable : ICallable
+{
+    private readonly Func<int, bool> _innerCallable;
 
-        public ClassCallable(Func<int, bool> innerCallable)
-        {
-            _innerCallable = innerCallable;
-        }
+    public ClassCallable(Func<int, bool> innerCallable)
+        => _innerCallable = innerCallable;
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public bool Call(int i) => _innerCallable(i);
-    }
+    public bool Call(int i) => _innerCallable(i);
+}
 
-    public struct StructCallable : ICallable
-    {
-        private readonly Func<int, bool> _innerCallable;
+public struct StructCallable : ICallable
+{
+    private readonly Func<int, bool> _innerCallable;
 
-        public StructCallable(Func<int, bool> innerCallable)
-        {
-            _innerCallable = innerCallable;
-        }
+    public StructCallable(Func<int, bool> innerCallable)
+        => _innerCallable = innerCallable;
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public bool Call(int i) => _innerCallable(i);
-    }
+    public bool Call(int i) => _innerCallable(i);
+}
 
-    public class InterfaceCaller
-    {
-        private const int SomeInt = 1;
+public class InterfaceCaller
+{
+    private const int SomeInt = 1;
 
-        private InterfaceCaller()
-        {
-        }
+    public void Call(ICallable callable)
+        => callable.Call(SomeInt);
+}
 
-        public static InterfaceCaller Instance { get; } = new InterfaceCaller();
+public class GenericCaller
+{
+    private const int SomeInt = 1;
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public void Call(ICallable callable) => callable.Call(SomeInt);
-    }
-
-    public class GenericCaller
-    {
-        private const int SomeInt = 1;
-
-        private GenericCaller()
-        {
-        }
-
-        public static GenericCaller Instance { get; } = new GenericCaller();
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public void Call<TCallable>(TCallable callable) where TCallable : ICallable
-            => callable.Call(SomeInt);
-    }
+    public void Call<TCallable>(TCallable callable) where TCallable : ICallable
+        => callable.Call(SomeInt);
 }
--- src/GenericsStructsAndInterfacesAsParameters/readme.md
+++ src/GenericsStructsAndInterfacesAsParameters/readme.md
@@ -7,15 +7,18 @@ This benchmark application illustrates the differences between these approaches.
 ## Results
 
 ``` ini
-BenchmarkDotNet=v0.12.0, OS=Windows 10.0.18363
+
+BenchmarkDotNet=v0.13.1, OS=Windows 10.0.19043.1237 (21H1/May2021Update)
 Intel Core i7-9700K CPU 3.60GHz (Coffee Lake), 1 CPU, 8 logical and 8 physical cores
-.NET Core SDK=3.1.100
-  [Host]     : .NET Core 3.1.0 (CoreCLR 4.700.19.56402, CoreFX 4.700.19.56404), X64 RyuJIT
-  DefaultJob : .NET Core 3.1.0 (CoreCLR 4.700.19.56402, CoreFX 4.700.19.56404), X64 RyuJIT
+.NET SDK=6.0.100-rc.1.21463.6
+  [Host]     : .NET 6.0.0 (6.0.21.45113), X64 RyuJIT
+  DefaultJob : .NET 6.0.0 (6.0.21.45113), X64 RyuJIT
+
+
 ```
-|                    Method |     Mean |     Error |    StdDev | Ratio | Rank |  Gen 0 | Gen 1 | Gen 2 | Allocated |
-|-------------------------- |---------:|----------:|----------:|------:|-----:|-------:|------:|------:|----------:|
-|  InterfaceCallerWithClass | 6.902 ns | 0.0536 ns | 0.0502 ns |  1.00 |    2 | 0.0038 |     - |     - |      24 B |
-| InterfaceCallerWithStruct | 7.515 ns | 0.0371 ns | 0.0347 ns |  1.09 |    4 | 0.0038 |     - |     - |      24 B |
-|    GenericCallerWithClass | 7.235 ns | 0.0740 ns | 0.0693 ns |  1.05 |    3 | 0.0038 |     - |     - |      24 B |
-|   GenericCallerWithStruct | 3.540 ns | 0.0094 ns | 0.0083 ns |  0.51 |    1 |      - |     - |     - |         - |
+|                    Method |     Mean |     Error |    StdDev | Ratio | RatioSD | Rank |  Gen 0 | Allocated |
+|-------------------------- |---------:|----------:|----------:|------:|--------:|-----:|-------:|----------:|
+|  InterfaceCallerWithClass | 4.941 ns | 0.0916 ns | 0.0812 ns |  1.00 |    0.00 |    2 | 0.0038 |      24 B |
+| InterfaceCallerWithStruct | 4.913 ns | 0.0451 ns | 0.0422 ns |  0.99 |    0.02 |    2 | 0.0038 |      24 B |
+|    GenericCallerWithClass | 5.277 ns | 0.0623 ns | 0.0583 ns |  1.07 |    0.02 |    3 | 0.0038 |      24 B |
+|   GenericCallerWithStruct | 1.407 ns | 0.0056 ns | 0.0053 ns |  0.28 |    0.00 |    1 |      - |         - |
