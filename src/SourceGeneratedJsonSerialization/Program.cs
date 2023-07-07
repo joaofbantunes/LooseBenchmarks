95f6697fb8b7984d68f9c197d60125bf96cd7cb7
João Antunes <joao@antunes.dev>
2023-07-07 10:55:46 +0100

    Adding a couple of new benchmarks

---
+++ src/SourceGeneratedJsonSerialization/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+_ = BenchmarkRunner.Run<Benchmark>();
+
+// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation
+
+[RankColumn]
+[MemoryDiagnoser]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+public class Benchmark
+{
+    private SomeRecordToSerialize[] _records = null!;
+
+    [Params(1, 10, 100, 1_000, 10_000, 100_000)]
+    public int N;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+        => _records = Enumerable
+            .Range(0, N)
+            .Select(i => new SomeRecordToSerialize(
+                i,
+                $"Some text {i}",
+                DateOnly.FromDayNumber(i),
+                Enumerable
+                    .Range(0, 10)
+                    .ToDictionary(j => $"Some Key {i}-{j}", j => $"Some Value {i}-{j}")))
+            .ToArray();
+
+    [Benchmark(Baseline = true)]
+    public string NotSourceGenerated() => JsonSerializer.Serialize(_records);
+
+    [Benchmark]
+    public string SourceGenerated()
+        => JsonSerializer.Serialize(_records, BenchmarkJsonSerializerContext.Default.SomeRecordToSerializeArray);
+}
+
+public record SomeRecordToSerialize(
+    int Id,
+    string Text,
+    DateOnly Date,
+    Dictionary<string, string> Map);
+
+[JsonSerializable(typeof(SomeRecordToSerialize[]))]
+[JsonSerializable(typeof(SomeRecordToSerialize))]
+internal partial class BenchmarkJsonSerializerContext : JsonSerializerContext
+{
+}
---
+++ src/ToListVsToArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+_ = BenchmarkRunner.Run<Benchmark>();
+
+[RankColumn]
+[MemoryDiagnoser]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+public class Benchmark
+{
+    [Params(1, 10, 100, 1_000, 10_000, 100_000)]
+    public int N;
+    
+    [Benchmark(Baseline = true)]
+    public IReadOnlyCollection<int> ToList()
+        => Enumerable.Range(0, N).ToList();
+
+    [Benchmark]
+    public IReadOnlyCollection<int> ToArray()
+        => Enumerable.Range(0, N).ToArray();
+    
+}
